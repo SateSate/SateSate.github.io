8a490c0f1246d9dc71f6876b5e15d98a777a01c5
Albi Cenga <albi.cenga@jakala.com>
2023-11-16 01:13:22 +0100

    nav fix

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -1,9 +1,13 @@
 const burgerBtn = document.querySelector('.burger-menu');
 const nav = document.querySelector('.navbar');
 const overlay = document.querySelector('.overlay');
+const menu = document.querySelector('.menu');
+const body = document.querySelector('body');
 
 burgerBtn.addEventListener('click', () => {
   burgerBtn.classList.toggle('active');
   nav.classList.toggle('open');
   overlay.classList.toggle('active');
+  menu.classList.toggle('overflow');
+  body.classList.toggle('overflow-h');
 });
